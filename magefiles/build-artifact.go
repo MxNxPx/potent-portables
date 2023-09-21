5c68667089d74227910a7f7586d82481b8d594ea
MxNxPx <MxNxPx@gmail.com>
2023-09-21 10:27:51 -0400

    feat: more targets for practice

--- magefiles/build-artifact.go
+++ magefiles/build-artifact.go
@@ -9,6 +9,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
@@ -16,15 +17,28 @@ import (
 
 var zarf = sh.RunCmd("zarf")
 
-// Zarf version
-func Zarf() error {
-	return zarf("versions")
+// Create package using Zarf
+func Build() error {
+	return zarf("package", "create", "--confirm", "--output", "./app", "./app")
 }
 
-// init example
-// func init() {
-// 	return ""
-// }
+// Install package using Zarf
+func Deploy() error {
+	os.Chdir("./app")
+	newDir, err := os.Getwd()
+	if err != nil {
+	}
+	fmt.Printf("Current Working Directory: %s\n", newDir)
+	filenamePattern := "zarf-package-*.tar.zst" // Change this to your desired wildcard pattern
+
+	filename, err := findFirstFileWithWildcard("./", filenamePattern)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return err
+	}
+
+	return zarf("package", "deploy", "--confirm", filename)
+}
 
 func runWith(env map[string]string, cmd string, inArgs ...any) error {
 	s := argsToStrings(inArgs...)
@@ -61,4 +75,21 @@ func argsToStrings(v ...any) []string {
 	}
 
 	return args
+}
+
+func findFirstFileWithWildcard(dir, wildcard string) (string, error) {
+	// Use filepath.Glob to list files that match the wildcard in the given directory
+	matches, err := filepath.Glob(filepath.Join(dir, wildcard))
+	if err != nil {
+		return "", err
+	}
+
+	// Check if there are any matches
+	if len(matches) == 0 {
+		return "", fmt.Errorf("No matching files found")
+	}
+
+	// Extract the first matching filename without the directory
+	filename := filepath.Base(matches[0])
+	return filename, nil
 }
